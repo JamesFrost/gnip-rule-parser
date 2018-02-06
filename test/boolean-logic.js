d81e61fe7f2bbf0c16e5482a4598aab385a9271a
James Frost <james.frost@mydrivesolutions.com>
2018-02-06 13:59:27 +0000

    Added exact match boolean operator

--- test/boolean-logic.js
+++ test/boolean-logic.js
@@ -222,7 +222,64 @@ describe('#match', function()
 
     describe('Exact Phrase Match', function()
     {
-      // TODO
+      describe('"call gnip"', function()
+      {
+        const rule = '"call gnip"';
+        const ast = parser.parse(rule);
+
+        describe('matches', function()
+        {
+          const expectedMatch = true;
+          const tweetData =
+          [
+            {
+              text: 'I need to call gnip, again'
+            },
+            {
+              text: 'I need to call gnip again'
+            },
+            {
+              text: 'call gnip'
+            }
+          ];
+
+          tweetData.map(function(thisTweetData)
+          {
+            it(thisTweetData.text, function()
+            {
+              testAst(ast, getTweetObject(thisTweetData), expectedMatch)
+            });
+          });
+        });
+
+        describe('doesn\'t match', function()
+        {
+          const expectedMatch = false;
+          const tweetData =
+          [
+            {
+              text: 'I called gnip'
+            },
+            {
+              text: 'call  gnip'
+            },
+            {
+              text: 'call-gnip'
+            },
+            {
+              text: 'call_gnip'
+            }
+          ];
+
+          tweetData.map(function(thisTweetData)
+          {
+            it(thisTweetData.text, function()
+            {
+              testAst(ast, getTweetObject(thisTweetData), expectedMatch)
+            });
+          });
+        });
+      });
     });
 
     describe('Proximity Operator', function()
