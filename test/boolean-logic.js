c2eb321f7f337ac947f84cd61b9924722aafbdd6
James Frost <james.frost@mydrivesolutions.com>
2018-02-06 13:52:33 +0000

    Added placeholder tests for matching boolean operators

--- test/boolean-logic.js
+++ test/boolean-logic.js
@@ -214,5 +214,250 @@ describe('#match', function()
         });
       });
     });
+
+    describe('Emoji', function()
+    {
+      // TODO
+    });
+
+    describe('Exact Phrase Match', function()
+    {
+      // TODO
+    });
+
+    describe('Proximity Operator', function()
+    {
+      // TODO
+    });
+
+    describe('Contains', function()
+    {
+      // TODO
+    });
+
+    describe('From', function()
+    {
+      // TODO
+    });
+
+    describe('To', function()
+    {
+      // TODO
+    });
+
+    describe('Url', function()
+    {
+      // TODO
+    });
+
+    describe('Url Title', function()
+    {
+      // TODO
+    });
+
+    describe('Url Description', function()
+    {
+      // TODO
+    });
+
+    describe('Url Contains', function()
+    {
+      // TODO
+    });
+
+    describe('Has Links', function()
+    {
+      // TODO
+    });
+
+    describe('Sample', function()
+    {
+      // TODO
+    });
+
+    describe('#', function()
+    {
+      // TODO
+    });
+
+    describe('Point Radius', function()
+    {
+      // TODO
+    });
+
+    describe('Bounding Box', function()
+    {
+      // TODO
+    });
+
+    describe('@', function()
+    {
+      // TODO
+    });
+
+    describe('$', function()
+    {
+      // TODO
+    });
+
+    describe('Bio', function()
+    {
+      // TODO
+    });
+
+    describe('Bio Name', function()
+    {
+      // TODO
+    });
+
+    describe('Retweets Of', function()
+    {
+      // TODO
+    });
+
+    describe('Language', function()
+    {
+      // TODO
+    });
+
+    describe('Bio Location', function()
+    {
+      // TODO
+    });
+
+    describe('Time Zone', function()
+    {
+      // TODO
+    });
+
+    describe('Statuses Count', function()
+    {
+      // TODO
+    });
+
+    describe('Followers Count', function()
+    {
+      // TODO
+    });
+
+    describe('Friends Count', function()
+    {
+      // TODO
+    });
+
+    describe('Listed Count', function()
+    {
+      // TODO
+    });
+
+    describe('Is Verified', function()
+    {
+      // TODO
+    });
+
+    describe('Source', function()
+    {
+      // TODO
+    });
+
+    describe('Place', function()
+    {
+      // TODO
+    });
+
+    describe('Place Country', function()
+    {
+      // TODO
+    });
+
+    describe('Has Geo', function()
+    {
+      // TODO
+    });
+
+    describe('Has Mentions', function()
+    {
+      // TODO
+    });
+
+    describe('Has Hashtags', function()
+    {
+      // TODO
+    });
+
+    describe('Has Images', function()
+    {
+      // TODO
+    });
+
+    describe('Has Videos', function()
+    {
+      // TODO
+    });
+
+    describe('Has Media', function()
+    {
+      // TODO
+    });
+
+    describe('Has Symbols', function()
+    {
+      // TODO
+    });
+
+    describe('Is Retweet', function()
+    {
+      // TODO
+    });
+
+    describe('Is Quote', function()
+    {
+      // TODO
+    });
+
+    describe('Retweet Of Status', function()
+    {
+      // TODO
+    });
+
+    describe('In Reply To Status', function()
+    {
+      // TODO
+    });
+
+    describe('Has Profile Geo', function()
+    {
+      // TODO
+    });
+
+    describe('Profile Point Radius', function()
+    {
+      // TODO
+    });
+
+    describe('Profile Bounding Box', function()
+    {
+      // TODO
+    });
+
+    describe('Profile Country', function()
+    {
+      // TODO
+    });
+
+    describe('Profile Region', function()
+    {
+      // TODO
+    });
+
+    describe('Profile Locality', function()
+    {
+      // TODO
+    });
+
+    describe('Profile Subregion', function()
+    {
+      // TODO
+    });
   });
 });
