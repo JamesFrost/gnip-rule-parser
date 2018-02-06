e2412c74c5f652c3918ab77b50b9bc3b9da5c79b
James Frost <james.frost@mydrivesolutions.com>
2018-02-06 13:42:41 +0000

    Work on matching keyword operator

--- test/boolean-logic.js
+++ test/boolean-logic.js
@@ -35,6 +35,15 @@ describe('#match', function()
           [
             {
               text: 'I need to call gnip'
+            },
+            {
+              text: 'Check out gnip\'s documentation.'
+            },
+            {
+              text: 'I love the @gnip blog.'
+            },
+            {
+              text: 'Check out Gnip.'
             }
           ];
 
@@ -49,22 +58,157 @@ describe('#match', function()
 
         describe('doesn\'t match', function()
         {
-          describe('matches', function()
-          {
-            const expectedMatch = false;
-            const tweetData =
-            [
-              {
-                text: '#gniprocks'
-              }
-            ];
-
-            tweetData.map(function(thisTweetData)
+          const expectedMatch = false;
+          const tweetData =
+          [
             {
-              it(thisTweetData.text, function()
-              {
-                testAst(ast, getTweetObject(thisTweetData), expectedMatch)
-              });
+              text: '#gniprocks'
+            }
+          ];
+
+          tweetData.map(function(thisTweetData)
+          {
+            it(thisTweetData.text, function()
+            {
+              testAst(ast, getTweetObject(thisTweetData), expectedMatch)
+            });
+          });
+        });
+      });
+
+      describe('cola', function()
+      {
+        const rule = 'cola';
+        const ast = parser.parse(rule);
+
+        describe('matches', function()
+        {
+          const expectedMatch = true;
+          const tweetData =
+          [
+            {
+              text: 'Ice cold cola on a hot day'
+            },
+            {
+              text: 'I like coca-cola!'
+            }
+          ];
+
+          tweetData.map(function(thisTweetData)
+          {
+            it(thisTweetData.text, function()
+            {
+              testAst(ast, getTweetObject(thisTweetData), expectedMatch)
+            });
+          });
+        });
+
+        describe('doesn\'t match', function()
+        {
+          const expectedMatch = false;
+          const tweetData =
+          [
+            {
+              text: 'I like cocacola!'
+            }
+          ];
+
+          tweetData.map(function(thisTweetData)
+          {
+            it(thisTweetData.text, function()
+            {
+              testAst(ast, getTweetObject(thisTweetData), expectedMatch)
+            });
+          });
+        });
+      });
+
+      describe('snow', function()
+      {
+        const rule = 'snow';
+        const ast = parser.parse(rule);
+
+        describe('matches', function()
+        {
+          const expectedMatch = true;
+          const tweetData =
+          [
+            {
+              text: 'please let it snow!'
+            },
+            {
+              text: 'https://en.wikipedia.org/wiki/Snow'
+            }
+          ];
+
+          tweetData.map(function(thisTweetData)
+          {
+            it(thisTweetData.text, function()
+            {
+              testAst(ast, getTweetObject(thisTweetData), expectedMatch)
+            });
+          });
+        });
+
+        describe('doesn\'t match', function()
+        {
+          const expectedMatch = false;
+          const tweetData =
+          [
+            {
+              text: 'it is finally snowing!'
+            }
+          ];
+
+          tweetData.map(function(thisTweetData)
+          {
+            it(thisTweetData.text, function()
+            {
+              testAst(ast, getTweetObject(thisTweetData), expectedMatch)
+            });
+          });
+        });
+      });
+
+      describe('Coachella', function()
+      {
+        const rule = 'Coachella';
+        const ast = parser.parse(rule);
+
+        describe('matches', function()
+        {
+          const expectedMatch = true;
+          const tweetData =
+          [
+            {
+              text: 'Hanging out at #coachella'
+            }
+          ];
+
+          tweetData.map(function(thisTweetData)
+          {
+            it(thisTweetData.text, function()
+            {
+              testAst(ast, getTweetObject(thisTweetData), expectedMatch)
+            });
+          });
+        });
+
+        describe('doesn\'t match', function()
+        {
+          const expectedMatch = false;
+          const tweetData =
+          [
+            {
+              text: 'NEW.PICS.FROM.COACHELLA2015!'
+            }
+          ];
+
+          tweetData.map(function(thisTweetData)
+          {
+            it(thisTweetData.text, function()
+            {
+              testAst(ast, getTweetObject(thisTweetData), expectedMatch)
             });
           });
         });
